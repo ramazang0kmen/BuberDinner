bcacbbf0ccd951d73ef0e27de44e92da3fe4dbfd
Ramazan GÖKMEN <ramazangokmen@aol.com>
2024-07-22 15:50:28 +0300

    Implementing AggregateRoot, Entity, ValueObject

---
+++ BuberDinner.Domain/Common/Models/AgregateRoot.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuberDinner.Domain.Common.Models
+{
+    public abstract class AgregateRoot<TId> : Entity<TId>
+        where TId : notnull
+    {
+        protected AgregateRoot(TId id) : base(id)
+        {
+        }
+    }
+}
