92f133410e9faff2e4111494c48c76c69601df43
Ramazan GÖKMEN <ramazangokmen@aol.com>
2024-07-23 00:21:11 +0300

    Domain Layer Structure & Skeleton

---
+++ BuberDinner.Domain/Common/Models/AggregateRoot .cs
@@ -0,0 +1,10 @@
+﻿namespace BuberDinner.Domain.Common.Models
+{
+    public abstract class AggregateRoot<TId> : Entity<TId>
+        where TId : notnull
+    {
+        public AggregateRoot(TId id) : base(id)
+        {
+        }
+    }
+}
